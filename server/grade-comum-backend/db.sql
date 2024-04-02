d37d5848b371e5b5cbf1117c759812047c1c3a01
b1guuuu <costa.igor0108@gmail.com>
2024-04-01 22:12:24 -0300

    [backend] adicionado arquivo com estrutura do banco em sql

---
+++ server/grade-comum-backend/db.sql
@@ -0,0 +1,60 @@
+CREATE TABLE IF NOT EXISTS disciplina(
+    id INT NOT NULL AUTO_INCREMENT,
+    nome TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS aluno (
+    id INT NOT NULL,
+    nome TEXT NOT NULL,
+    matricula VARCHAR(10) NOT NULL UNIQUE,
+    senha TEXT NOT NULL,
+    senhaSalt TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS professor(
+    id INT NOT NULL AUTO_INCREMENT,
+    nome TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS presenca(
+    id INT NOT NULL AUTO_INCREMENT,
+    presente BOOLEAN NOT NULL,
+    observacao TEXT DEFAULT '',
+    idProfessor INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idProfessor) REFERENCES professor(id)
+);
+
+CREATE TABLE IF NOT EXISTS turma (
+    id INT NOT NULL,
+    codigo INT NOT NULL,
+    numero INT NOT NULL,
+    idDisciplina INT NOT NULL,
+    idProfessor INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idDisciplina) REFERENCES disciplina(id),
+    FOREIGN KEY (idProfessor) REFERENCES professor(id)
+);
+
+CREATE TABLE IF NOT EXISTS horario (
+    id INT NOT NULL,
+    diaSemana VARCHAR(30) NOT NULL,
+    inicio VARCHAR(5) NOT NULL,
+    fim VARCHAR(5) NOT NULL,
+    sala VARCHAR(5) NOT NULL,
+    idTurma INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (idTurma) REFERENCES turma(id)
+);
+
+CREATE TABLE IF NOT EXISTS inscricao (
+    idTurma INT NOT NULL,
+    idAluno INT NOT NULL,
+    FOREIGN KEY (idTurma) REFERENCES turma(id),
+    FOREIGN KEY (idAluno) REFERENCES aluno(id),
+    PRIMARY KEY (idTurma, idAluno)
+);
+
