365ce524be1e5ae750b7594834963f466d8619a1
b1guuuu <costa.igor0108@gmail.com>
2024-05-12 22:50:55 -0300

    [backend] atualizadas referências de on delete e on update

--- server/grade-comum-backend/src/util/definirRelacoes.js
+++ server/grade-comum-backend/src/util/definirRelacoes.js
@@ -11,59 +11,59 @@ const Turma = require('../repository/turma')
 const Progresso = require('../repository/progresso')
 
 // Aluno <-> Inscrição
-Aluno.hasMany(Inscricao, { foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'alunoinscricao' })
-Inscricao.belongsTo(Aluno, { targetKey: 'id', foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'inscricaoaluno' })
+Aluno.hasMany(Inscricao, { foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'alunoinscricao', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Inscricao.belongsTo(Aluno, { targetKey: 'id', foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'inscricaoaluno', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Curso <-> Aluno
-Curso.hasMany(Aluno, { foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'cursoaluno' })
-Aluno.belongsTo(Curso, { targetKey: 'id', foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'alunocurso' })
+Curso.hasMany(Aluno, { foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'cursoaluno', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Aluno.belongsTo(Curso, { targetKey: 'id', foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'alunocurso', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Curso <-> Disciplina
-Curso.hasMany(Disciplina, { foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'cursodisciplina' })
-Disciplina.belongsTo(Curso, { targetKey: 'id', foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'disciplinacurso' })
+Curso.hasMany(Disciplina, { foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'cursodisciplina', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Disciplina.belongsTo(Curso, { targetKey: 'id', foreignKey: { name: 'idCurso', field: 'idcurso' }, as: 'disciplinacurso', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Disciplina <-> Turma
-Disciplina.hasMany(Turma, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinaturma' })
-Turma.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'turmadisciplina' })
+Disciplina.hasMany(Turma, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinaturma', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Turma.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'turmadisciplina', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Disciplina <-> Requisito (Base)
-Disciplina.hasMany(Requisito, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinabaserequisito' })
-Requisito.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'requisitodisciplinabase' })
+Disciplina.hasMany(Requisito, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinabaserequisito', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Requisito.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'requisitodisciplinabase', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Disciplina <-> Requisito (Disciplina Requisito)
-Disciplina.hasMany(Requisito, { foreignKey: { name: 'idDisciplinaRequisito', field: 'iddisciplinarequisito' }, as: 'disciplinarequisito' })
-Requisito.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplinaRequisito', field: 'iddisciplinarequisito' }, as: 'requisitodisciplina' })
+Disciplina.hasMany(Requisito, { foreignKey: { name: 'idDisciplinaRequisito', field: 'iddisciplinarequisito' }, as: 'disciplinarequisito', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Requisito.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplinaRequisito', field: 'iddisciplinarequisito' }, as: 'requisitodisciplina', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Horario <-> Turma
-Horario.belongsTo(Turma, { targetKey: 'id', foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'horarioturma' })
-Turma.hasMany(Horario, { foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'turmahorario' })
+Horario.belongsTo(Turma, { targetKey: 'id', foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'horarioturma', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Turma.hasMany(Horario, { foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'turmahorario', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Inscricao <-> Turma
-Inscricao.belongsTo(Turma, { targetKey: 'id', foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'inscricaoturma' })
-Turma.hasMany(Inscricao, { foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'turmainscricao' })
+Inscricao.belongsTo(Turma, { targetKey: 'id', foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'inscricaoturma', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Turma.hasMany(Inscricao, { foreignKey: { name: 'idTurma', field: 'idturma' }, as: 'turmainscricao', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Presenca <-> Professor
-Presenca.belongsTo(Professor, { targetKey: 'id', foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'presencaprofessor' })
-Professor.hasOne(Presenca, { foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'professorpresenca' })
+Presenca.belongsTo(Professor, { targetKey: 'id', foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'presencaprofessor', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Professor.hasOne(Presenca, { foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'professorpresenca', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Professsor <-> Turma
-Professor.hasMany(Turma, { foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'professorturma' })
-Turma.belongsTo(Professor, { targetKey: 'id', foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'turmaprofessor' })
+Professor.hasMany(Turma, { foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'professorturma', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Turma.belongsTo(Professor, { targetKey: 'id', foreignKey: { name: 'idProfessor', field: 'idprofessor' }, as: 'turmaprofessor', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Aluno <-> Anotacao
-Aluno.hasMany(Anotacao, { foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'alunoanotacao' })
-Anotacao.belongsTo(Aluno, { targetKey: 'id', foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'anotacaoaluno' })
+Aluno.hasMany(Anotacao, { foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'alunoanotacao', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Anotacao.belongsTo(Aluno, { targetKey: 'id', foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'anotacaoaluno', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Disciplina <-> Anotacao
-Disciplina.hasMany(Anotacao, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinaanotacao' })
-Anotacao.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'anotacaodisciplina' })
+Disciplina.hasMany(Anotacao, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinaanotacao', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Anotacao.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'anotacaodisciplina', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Aluno <-> Inscrição
-Aluno.hasMany(Progresso, { foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'alunoprogresso' })
-Progresso.belongsTo(Aluno, { targetKey: 'id', foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'progressoaluno' })
+Aluno.hasMany(Progresso, { foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'alunoprogresso', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Progresso.belongsTo(Aluno, { targetKey: 'id', foreignKey: { name: 'idAluno', field: 'idaluno' }, as: 'progressoaluno', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 // Inscricao <-> Turma
-Progresso.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'progressodisciplina' })
-Disciplina.hasMany(Progresso, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinaprogresso' })
+Progresso.belongsTo(Disciplina, { targetKey: 'id', foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'progressodisciplina', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+Disciplina.hasMany(Progresso, { foreignKey: { name: 'idDisciplina', field: 'iddisciplina' }, as: 'disciplinaprogresso', onDelete: 'CASCADE', onUpdate: 'CASCADE' })
 
 console.log('Relações definidas')
