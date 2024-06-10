8297e1e02975478ae8379ee3b9bdde0e6abe6173
b1guuuu <costa.igor0108@gmail.com>
2024-06-10 18:22:20 -0300

    [mobile] listagem de presença dos professores no início

--- mobile/grade/lib/view/page/inicio.dart
+++ mobile/grade/lib/view/page/inicio.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grade/view/component/container_base.dart';
+import 'package:grade/view/component/listagem_presenca_professores.dart';
+import 'package:grade/view/component/my_container.dart';
 import 'package:grade/view/component/navegacao.dart';
 import 'package:grade/view/component/tabela_grade.dart';
 
@@ -17,6 +19,14 @@ class InicioPage extends StatefulWidget {
 class InicioPageState extends State<InicioPage> {
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+    var mainContainerWidth = screenWidth > 1050 ? 1050.0 : screenWidth * 0.9;
+    var mainContainerHeight =
+        mainContainerWidth == 1050.0 ? 320.0 : screenHeight * 0.9;
+
+    var contentContainerWidth = mainContainerWidth * 0.9;
+    var contentContainerHeight = mainContainerHeight * 0.9;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inicio'),
@@ -24,7 +34,45 @@ class InicioPageState extends State<InicioPage> {
       drawer: const Drawer(
         child: Navegacao(),
       ),
-      body: const ContainerBase(child: TabelaGrade(visualizacaoSemanal: false)),
+      body: ContainerBase(
+          child: Center(
+        child: MyContainer(
+            mainContainerWidth: mainContainerWidth,
+            mainContainerHeight: mainContainerHeight,
+            contentContainerWidth: contentContainerWidth,
+            contentContainerHeight: contentContainerHeight,
+            child: mainContainerWidth == 1050.0
+                ? Row(
+                    children: [
+                      SizedBox(
+                        width: contentContainerWidth * 0.49,
+                        child: const TabelaGrade(visualizacaoSemanal: false),
+                      ),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      SizedBox(
+                        width: contentContainerWidth * 0.49,
+                        child: const ListagemPresencaProfessores(),
+                      ),
+                    ],
+                  )
+                : Column(children: [
+                    SizedBox(
+                      height: contentContainerHeight * 0.49,
+                      width: contentContainerWidth - 10,
+                      child: const TabelaGrade(visualizacaoSemanal: false),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: contentContainerHeight * 0.49,
+                      width: contentContainerWidth - 10,
+                      child: const ListagemPresencaProfessores(),
+                    ),
+                  ])),
+      )),
     );
   }
 }
